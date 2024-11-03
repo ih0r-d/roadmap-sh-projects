3bc4649ff2a560c432a48663743e3189ad5ea490
Ihor Didyk <ihordidyk95@gmail.com>
2024-11-03 12:59:35 +0200

    init mvn project and sdlman env

---
+++ backend-projects/task-tracker/src/main/java/com/ih0rd/App.java
@@ -0,0 +1,13 @@
+package com.ih0rd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
